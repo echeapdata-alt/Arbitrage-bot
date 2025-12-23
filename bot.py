c3a77ada42bfc4d3b28ac5414337eb24b0a7208f
echeapdata-alt <echeapdata@gmail.com>
2025-12-23 20:19:54 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "https://api.binance.com/api/v3/ticker/price"
+prices = requests.get(url).json()
+
+for coin in prices[:10]:  # just showing first 10 coins for testing
+    print(coin["symbol"], coin["price"])
